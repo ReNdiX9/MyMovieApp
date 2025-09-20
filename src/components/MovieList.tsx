50424dde1e0ef415b9863744e1e644e9f4a115b8
Yehor Rudiakov <egor.rudiakov@gmai.com>
2025-09-20 11:29:09 -0600

    Set movies to store

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
-import { type Movie } from "@/lib/types";
+
 import MovieCard from "./MovieCard";
+import { useSearchStore } from "@/store/store";
 
 export default function MovieList() {
-  const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+
+  //store variables
+  const movies = useSearchStore((s) => s.movies);
+  const setMovies = useSearchStore((s) => s.setMovies);
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -13,19 +17,20 @@ export default function MovieList() {
         const res = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=2f06df6daf4f704fffa29a95cf1f8393");
         const data = await res.json();
         setMovies(data.results);
+        console.log("Movies set to store:", data.results);
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
       } catch (error: any) {
+        console.error("Failed to fetch movies for store:", error);
         setError(error.message ?? "Failed to fetch");
       } finally {
         setLoading(false);
       }
     };
-
     fetchMovies();
   }, []);
 
-  if (loading) return <p className="p-4">Loading movies…</p>;
-  if (error) return <p className="p-4 text-red-600">Error: {error}</p>;
+  if (loading) return <p className="p-4 text-center text-2xl">Loading movies…</p>;
+  if (error) return <p className="p-4 text-red-600 text-center text-2xl">Error: {error}</p>;
 
   return (
     <section className="grid gap-6  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 px-2">
